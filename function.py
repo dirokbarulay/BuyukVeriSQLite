17664567fb14ed092ce1e7a377207d158a39ee23
Dirok <95727909+dirokbarulay@users.noreply.github.com>
2021-12-08 16:19:15 +0300

    Update function.py

--- function.py
+++ function.py
@@ -1,4 +1,4 @@
-def listele(database1,liste):
+rdef listele(database1,liste):
     islem = database1.cursor()
     komut = liste
     islem.execute(komut)
@@ -7,8 +7,20 @@ def listele(database1,liste):
 def veri_ekle(database1, ekleme):
     kayit = database1.cursor()
     veriler= ekleme
-    kayit.execute(kayit)
+    kayit.execute(veriler)
     return kayit
+
+def veri_silme(database1, silme):
+    kayit2 = database1.cursor()
+    veriler2= silme
+    kayit2.execute(veriler2)
+    return kayit2
+
+def veri_guncelle(database1, guncelle):
+    kayit3 = database1.cursor()
+    veriler3= guncelle
+    kayit3.execute(veriler3)
+    return kayit3
 
 def veri_getir(database1,sorgu):
     islem2 =database1.cursor()
